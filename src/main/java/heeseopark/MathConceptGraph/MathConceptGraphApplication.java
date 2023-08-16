2478f7ff347184574d9cf531197a2cac970b5c9d
heeseopark <heeseopark@korea.ac.kr>
2023-08-17 00:32:54 +0900

    Project initial setting

---
+++ src/main/java/heeseopark/MathConceptGraph/MathConceptGraphApplication.java
@@ -0,0 +1,13 @@
+package heeseopark.MathConceptGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathConceptGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathConceptGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heeseopark/MathConceptGraph/MathConceptGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package heeseopark.MathConceptGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathConceptGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
